bd8e160fd90125eb9bc5a2462339fce84057f29a
Avi-17 <workhorseavi17@gmail.com>
2024-11-28 20:22:31 +0530

    1

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,32 +1,40 @@
-const {prisma} = require("../db/config");
+const { prisma } = require("../db/config");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
-const signUpController = async(req, res) => {
+const signUpController = async (req, res) => {
+  try {
     const { name, email, password } = req.body;
-    if(!email) return res.status(400).json({error: "Email is required"});
-    if(!password) return res.status(400).json({error: "Password is required"});
+    if (!email) return res.status(400).json({ error: "Email is required" });
+    if (!password)
+      return res.status(400).json({ error: "Password is required" });
 
-    const user = await prisma.User.findUnique({where: {email}});
-    if(user) return res.status(400).json({error: "Email already in use"});
+    const user = await prisma.User.findUnique({ where: { email } });
+    if (user) return res.status(400).json({ error: "Email already in use" });
 
     const hashedPass = await bcrypt.hash(password, 10);
 
-    const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: "1h"})
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
     const newUser = await prisma.User.create({
-        data: {
-            name: name,
-            email: email,
-            password: hashedPass
-        }
+      data: {
+        name: name,
+        email: email,
+        password: hashedPass,
+      },
     });
     return res.status(201).json({
-        message: "User created successfully",
-        userId: newUser.id
-    })
+      message: "User created successfully",
+      userId: newUser.id,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = signUpController;
